6317a763fccf219eb35ad1f521d4ae5d3cccbdbe
Daisy Ramos <daisyramos317@gmail.com>
2021-02-01 16:34:41 -0500

    Initial Commit

---
+++ DynamicTypeDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicTypeDemo
+//
+//  Created by Daisy Ramos on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
